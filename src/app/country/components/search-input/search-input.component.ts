c986b6c68fb73e1c1040294d59f14fe15307b812
gpena87 <gpenagutierrez87@gmail.com>
2026-06-09 19:34:53 -0400

    fix

---
+++ src/app/country/components/search-input/search-input.component.ts
@@ -0,0 +1,11 @@
+import { Component, output, input } from '@angular/core';
+
+@Component({
+  selector: 'country-search-input',
+  imports: [],
+  templateUrl: './search-input.component.html',
+})
+export class SearchInputComponent {
+  placeholder = input('Buscar');
+  value = output<string>();
+}
--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page.component/by-capital-page.component';
 import { CountryLayoutComponent } from './layouts/countryLayout.component/countryLayout.component';
+import { ByCountrycomponent } from './pages/by-country/by-country.component';
+import { ByRegioncomponent } from './pages/by-region/by-region.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
 
 export const countryRoutes: Routes = [
   {
@@ -10,6 +13,18 @@ export const countryRoutes: Routes = [
       {
         path: 'by-capital',
         component: ByCapitalPageComponent
+      },
+      {
+        path: 'by-country',
+        component: ByCountrycomponent
+      },
+      {
+        path: 'by-region',
+        component: ByRegioncomponent
+      },
+      {
+        path: 'by/:code',
+        component: CountryPageComponent
       },
       {
         path: '**',
